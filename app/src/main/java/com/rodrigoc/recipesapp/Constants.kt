d9c532e31b57b597f11f4c470e7d23569e0e8bdc
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2021-11-20 11:03:00 -0300

    create constants class

---
+++ app/src/main/java/com/rodrigoc/recipesapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.rodrigoc.recipesapp
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com/"
+        const val API_KEY = "239fd4dd8b174d6f8b3899d1b77556bf"
+    }
+}
