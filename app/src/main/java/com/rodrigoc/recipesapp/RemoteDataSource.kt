874de36e20a5c835737ce7405361d8b71e87d7fe
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2021-11-20 16:15:00 -0300

    Inject RemoteDataSource::class

--- app/src/main/java/com/rodrigoc/recipesapp/RemoteDataSource.kt
+++ app/src/main/java/com/rodrigoc/recipesapp/RemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.rodrigoc.recipesapp
 
-class RemoteDataSource() {
+import com.rodrigoc.recipesapp.model.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi,
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
 }
